c908320bf911a155c2470bd6894edb2ea433b819
saqq121 <85352642+saqq121@users.noreply.github.com>
2022-09-06 14:09:19 +0400

    finish

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import "../src/App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MenusPage from "../src/Pages/MenusPage";
 import HomePage from "./Pages/HomePage";
+import ScrollTop from "./ScrollTop";
 
 export default function App() {
   return (
     <Router>
+      <ScrollTop />
+
       <div className="container">
         <Routes>
           <Route path="/" element={<HomePage />}></Route>
---
+++ src/ScrollTop.jsx
@@ -0,0 +1,15 @@
+import { useEffect } from "react";
+import {
+    BrowserRouter as Router,
+    Switch,
+    useLocation
+  } from "react-router-dom";    
+
+export default function ScrollTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+}
