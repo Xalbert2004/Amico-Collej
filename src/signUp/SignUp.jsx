ee964dbc30252e70613e96ad1c35c54a29f5f906
Xalbert2004 <davtyanxalbert@gmail.com>
2022-12-27 23:15:07 +0400

    start backend

--- src/signUp/SignUp.jsx
+++ src/signUp/SignUp.jsx
@@ -1,21 +1,60 @@
 import React from 'react';
 import { useState } from 'react';
-import styles from '../signUp/signUp.module.scss'
+import styles from '../signUp/signUp.module.scss';
+import {   useNavigate } from 'react-router-dom';
+import axios from "axios";
+
+
 
 export default function SignUp({ setActive }) {
 
   const [login, setLogin] = useState(true);
+
+  let history = useNavigate();
+  const [data, setData] = useState({
+    name:"",
+    email:"",
+    password:"",
+  })
+  
+  const handleChange=(e)=>{
+    setData({...data, [e.target.name]: e.target.value});
+
+  }
+
+  const submitForm=(e)=>{
+    e.preventDefault();
+    const sendData = {
+      name:data.name,
+      email:data.email,
+      password:data.password
+    }
+
+    console.log(sendData);
+
+    axios.post('', sendData)
+    .then((result)=>{
+      if (result.data.Status === 'Invalid'){
+        alert('Invalid User');
+      }
+      else{
+        history(`/dashboard`)
+      }
+      
+    })
+  }
+
   return (
     <>
     {login ? (
         <div className={styles.modal} onClick={() => setActive(false) } >
           <div className={styles.modal_content} onClick={(e) => e.stopPropagation()} >
-          <form action="" className={styles.signUp}>
+          <form onSubmit={submitForm} className={styles.signUp}>
                 <p className={styles.title}>Գրանցում</p>
                 <p className={styles.text}>Գրանցվիր անվճար և օգտվիր մեր ծաոայություններից</p>
-                <input className={styles.inp_sign} type="text" placeholder='Login' />
-                <input className={styles.inp_sign2} type="password" placeholder='Password' />
-                <input className={styles.inp_sign2} type="email" placeholder='Email' />
+                <input onChange={handleChange} name="name" value={data.name} className={styles.inp_sign} type="text" placeholder='Login' />
+                <input onChange={handleChange} name="password" value={data.password} className={styles.inp_sign2} type="password" placeholder='Password' />
+                <input onChange={handleChange} name="email" value={data.email}  className={styles.inp_sign2} type="email" placeholder='Email' />
                 <div className={styles.chekb_block}>
                   <div>
                     <input  id="chbox" name="chbox" type="checkbox" />
